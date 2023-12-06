b43ee2c00484f803005d5bb92ac48da7cb192b54
JonFD4 <jonathanforsondanso@gmail.com>
2023-12-06 20:47:35 +0000

    Add function to design 2D array of board

--- run.py
+++ run.py
@@ -6,3 +6,33 @@ import random
 
 # Define constants
 ROW_COUNT, COLUMN_COUNT = 6, 7
+
+
+def create_board():
+    """
+    Create a 2d NumPY list matrix with dimensions specified by 
+    ROW_COUNT and COLUMN_COUNT.
+    The array is initialized with zeros, and dtype=int 
+    ensures that the elements of the array are of integer data type.
+    """
+    return np.zeros((ROW_COUNT, COLUMN_COUNT), dtype=int)
+
+def print_board(board):
+    """
+    Iterate over each row in the 2D list board.
+    Convert each element in the current row to string.
+    The space created using .join represents row as a space-spearated string.
+    Print all rows on a single line without square brackets.
+
+    """
+    for row in board:
+        print(' '.join(map(str, row)), end= '')
+    print()
+
+def play_game():
+    """
+    Runs the game
+    """
+    board = create_board()
+    print(board)
+play_game()
