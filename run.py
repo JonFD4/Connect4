c2142884f1dd8c56dfaf8a80fa0351984fc17973
JonFD4 <jonathanforsondanso@gmail.com>
2023-12-06 20:10:01 +0000

    Import necessary modules and libraries and defining constants

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import necessary modules
+import numpy as np
+import sys
+import math
+import random
+
+# Define constants
+ROW_COUNT, COLUMN_COUNT = 6, 7
